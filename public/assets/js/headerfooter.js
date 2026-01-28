00c167b42864a0781c9d5d859f70518c0c18535d
HARSH TIWARI <officialharshtiwari8435@gmail.com>
2026-01-28 16:36:45 +0530

    Update headerfooter.js

--- public/assets/js/headerfooter.js
+++ public/assets/js/headerfooter.js
@@ -1,7 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    // Initial render
-    applyFilters();
 
     // ================= HEADER LOAD =================
     fetch("header.html")
